3c1197573930854f5e4bf5586f467535a33f8ff6
gabmira14 <gmmscontato@gmail.com>
2025-07-10 11:23:01 -0300

    schema prisma.2

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
-const nextConfig = {
+import { NextConfig } from "next";
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
+const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+
     experimental: {
         outputFileTracingIgnores: ["./generated/client/**/*"],
     } as any,
-};
 
-export default nextConfig;
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
+        return config
+    },
+}
+
+export default nextConfig
+
